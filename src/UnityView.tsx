4573b89120cc792a9c83a554a4354e653d77b258
Jan Jędrychowski <jan.jedrychowski@globalapptesting.com>
2021-05-27 17:20:29 +0200

    Refactor UnityView

--- src/UnityView.tsx
+++ src/UnityView.tsx
@@ -1,98 +1,49 @@
 import * as React from "react";
-import { NativeModules, requireNativeComponent, ViewProps, View } from 'react-native';
-import * as PropTypes from "prop-types";
-const { ViewPropTypes } = require('react-native');
-import { UnityMessageHandler, UnityMessageHandlerImpl } from "./UnityMessageHandler";
+import { requireNativeComponent, ViewProps, View } from 'react-native';
+import { UnityMessageHandler } from "./UnityMessageHandler";
 import { UnityModule } from "./UnityModule";
-import { UnityMessageType, UnityMessage } from "./UnityMessage";
-import { IUnityRequest } from "./UnityRequest";
-import { Observable } from "rxjs";
 
-const { UIManager } = NativeModules;
+const { useState, useEffect } = React;
 
 export interface UnityViewProps extends ViewProps {
-    /** 
-     * Receive plain text message from unity. 
+    /**
+     * Receive plain text message from unity.
      */
     onMessage?: (message: string) => void;
 
-    /** 
-    * Receive JSON message or request from unity. 
+    /**
+    * Receive JSON message or request from unity.
     */
     onUnityMessage?: (handler: UnityMessageHandler) => void;
-}
-
-export default class UnityView extends React.Component<UnityViewProps> {
-    private m_registrationToken: number;
-
-    public static propTypes = {
-        ...ViewPropTypes,
-        onMessage: PropTypes.func
-    }
-
-    public constructor(props: any) {
-        super(props);
-
-        this.m_registrationToken = UnityModule.addMessageListener(message => {
-            if (this.props.onUnityMessage && message instanceof UnityMessageHandlerImpl) {
-                this.props.onUnityMessage(message);
-            }
-            if (this.props.onMessage && typeof message === 'string') {
-                this.props.onMessage(message);
-            }
-        });
-    }
-
-    public componentWillUnmount() {
-        UnityModule.removeMessageListener(this.m_registrationToken);
-        UnityModule.clear();
-    }
-
-    /**
-     * [Deprecated] Use `UnityModule.pause` instead.
-     */
-    public pause() {
-        UnityModule.pause();
-    };
-
-    /**
-     * [Deprecated] Use `UnityModule.resume` instead.
-     */
-    public resume() {
-        UnityModule.resume();
-    };
-
-    /**
-     * [Deprecated] Use `UnityModule.postMessage` instead.
-     */
-    public postMessage(message: string | UnityMessage, gameObject?: string, methodName?: string): void {
-        UnityModule.postMessage(message, gameObject, methodName);
-    };
-
-    /**
-     * [Deprecated] Use `UnityModule.postMessageAsync` instead.
-     */
-    public postMessageAsync<TResponse = any, TType extends number = UnityMessageType, TData = any>(request: IUnityRequest<TType, TData, TResponse>, gameObject?: string, methodName?: string): Observable<TResponse>;
-    public postMessageAsync<TResponse = any, TType extends number = UnityMessageType, TData = any>(id: string, data: any, gameObject?: string, methodName?: string): Observable<TResponse>;
-    public postMessageAsync<TResponse = any, TType extends number = UnityMessageType, TData = any>(id: string, type: TType, data: any, gameObject?: string, methodName?: string): Observable<TResponse>;
-    public postMessageAsync<TResponse = any, TType extends number = UnityMessageType, TData = any>(first: string | IUnityRequest<TType, TData, TResponse>, second: any, third: any, fourth?: string, fifth?: string): Observable<TResponse> {
-        return UnityModule.postMessageAsync(first as any, second, third, fourth, fifth);
-    }
-
-    public render() {
-        const { onUnityMessage, onMessage, ...props } = this.props;
-        return (
-            <View {...props}>
-                <NativeUnityView
-                    style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
-                    onUnityMessage={onUnityMessage}
-                    onMessage={onMessage}
-                >
-                </NativeUnityView>
-                {this.props.children}
-            </View>
-        );
-    }
+    children?: React.ReactNode[];
 }
 
 const NativeUnityView = requireNativeComponent<UnityViewProps>('UnityView');
+
+const UnityView = ({ onUnityMessage, onMessage, children, ...props }: UnityViewProps) => {
+    const [unitySubscription, setUnitySubscription] = useState<number | null>(null);
+    const [stringSubscription, setStringSubscription] = useState<number | null>(null);
+
+    useEffect(() => {
+        onUnityMessage && setUnitySubscription(UnityModule.addUnityMessageListener(onUnityMessage));
+        onMessage && setStringSubscription(UnityModule.addStringMessageListener(onMessage));
+
+        return () => {
+            unitySubscription && UnityModule.removeMessageListener(unitySubscription);
+            stringSubscription && UnityModule.removeMessageListener(stringSubscription);
+            UnityModule.clear();
+        }
+    }, []);
+
+    return (
+        <View {...props}>
+            <NativeUnityView
+                style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+            >
+            </NativeUnityView>
+            {children}
+        </View>
+    );
+}
+
+export default UnityView;
